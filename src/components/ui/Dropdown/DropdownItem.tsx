8f0673893ad5da44b5e10d94ee6a057f23bdff44
Adrian Sudak <adriansudak@outlook.com>
2024-07-27 22:09:06 +0200

    feat: Load custom cursors

--- src/components/ui/Dropdown/DropdownItem.tsx
+++ src/components/ui/Dropdown/DropdownItem.tsx
@@ -14,8 +14,12 @@ const DropdownItem = ({ item, className, setOpen }: Props) => {
     <li
       className={clsx(
         className,
-        "px-[1rem] py-[0.25rem] rounded-md cursor-pointer transition-colors",
-        "hover:bg-macos-blue-500 hover:text-white flex justify-between gap-14"
+        "px-[1rem] py-[0.25rem] rounded-md transition-colors  flex justify-between gap-14",
+        " ",
+        {
+          "hover:bg-macos-blue-500 hover:text-white text-white": !item.inactive,
+        },
+        { "text-gray-400": item.inactive }
       )}
       onClick={(e) => {
         e.preventDefault();
