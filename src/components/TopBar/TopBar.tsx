ad9af3d9759d62f9a557d03150bb6e36bc5a526c
Adrian Sudak <adriansudak@outlook.com>
2024-07-28 12:54:01 +0200

    feat: Add blure

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -10,11 +10,12 @@ const TopBar = ({ children }: PropsWithChildren) => {
     <div
       className={clsx(
         "w-full h-[1.75rem] px-[0.4rem] flex justify-between",
+        "before:backdrop-blur-xl before:absolute before:z-[-10px] before:left-0 before:top-0 before:w-full before:h-7",
         { "bg-black bg-opacity-[18%] text-white": darkMode },
         { "bg-white bg-opacity-50 text-black": !darkMode }
       )}
     >
-      {children}
+      <div className="z-[5] w-full  flex justify-between">{children}</div>
     </div>
   );
 };
