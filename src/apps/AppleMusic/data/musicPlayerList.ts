d20564ce630c76bb3bce00be0409095e67e12e7b
Adrian Sudak <adriansudak@outlook.com>
2024-08-03 23:54:03 +0200

    refactor: Moved types and music player data into a AppleMusic folder

---
+++ src/apps/AppleMusic/data/musicPlayerList.ts
@@ -0,0 +1,32 @@
+import { Song } from "../types/song";
+
+export const songs: Song[] = [
+  {
+    title: "Czekam na stacji",
+    artist: "TEN TONYE",
+    cover: "url(/src/apps/AppleMusic/assets/covers/czekam-na-stacji.jpg)",
+    href: "https://open.spotify.com/album/5hrNeUJXxNkzdHIOam255C?si=VihngdacQmmo6RbwVLfbdw",
+    src: "/src/apps/AppleMusic/assets/songs/czekam-na-stacji.mp3",
+  },
+  {
+    title: "KRUKI I WRONY",
+    artist: "TEN TONYE",
+    cover: "url(/src/apps/AppleMusic/assets/covers/kruki-i-wrony.jpg)",
+    href: "https://open.spotify.com/album/1DDqzg8Zbvpj8tVhrK3LBx?si=8-BnCadRSOeZ-KVce6eWcw",
+    src: "/src/apps/AppleMusic/assets/songs/kruki-i-wrony.mp3",
+  },
+  {
+    title: "Plaża POLAROID",
+    artist: "TEN TONYE",
+    cover: "url(/src/apps/AppleMusic/assets/covers/plaza-polaroid.jpg)",
+    href: "https://open.spotify.com/album/7y9mfqbtBp1A19PJUshv0Y?si=Hv5HY0gwSVSDMA60QyiY1w",
+    src: "/src/apps/AppleMusic/assets/songs/plaza-polaroid.mp3",
+  },
+  {
+    title: "Czekaj na mnie",
+    artist: "TEN TONYE / smxk / Gabriel...",
+    cover: "url(/src/apps/AppleMusic/assets/covers/czekaj-na-mnie.jpg)",
+    href: "https://open.spotify.com/album/1elNf5L7f0fqKCHYi2Vof0?si=IuQM8-04QXO-FCH-PrCD8Q",
+    src: "/src/apps/AppleMusic/assets/songs/czekaj-na-mnie.mp3",
+  },
+];
---
+++ src/apps/AppleMusic/types/song.ts
@@ -0,0 +1,7 @@
+export interface Song {
+  title: string;
+  artist: string;
+  cover: string;
+  href: string;
+  src: string;
+}
