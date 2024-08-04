6eae96b9dde51a1fc36ee80b0e9209e986734ae4
Adrian Sudak <adriansudak@outlook.com>
2024-08-04 02:30:24 +0200

    fix: Temporary delet finder folder

--- src/utils/getMenuByTitle.ts
+++ src/utils/getMenuByTitle.ts
@@ -1,4 +1,4 @@
-import { finderMenu } from "@/apps/dwadwa/finderMenu";
+import { finderMenu } from "@/apps/Finder/finderMenu";
 import { appsTitles } from "@/constants/apps";
 
 export const getMenuByTitle = (title: string) => {
