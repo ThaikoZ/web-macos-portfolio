eaab505d04363d7f92fc8911b3e081ea78042fbf
Adrian Sudak <adriansudak@outlook.com>
2024-08-05 00:03:29 +0200

    fix: Add lose active window if click on desktop

--- src/components/desktop/Desktop.tsx
+++ src/components/desktop/Desktop.tsx
@@ -1,21 +1,29 @@
+import { setActiveWindow } from "@/store/reducers/windowSlice";
 import { Store } from "@/store/store";
 import { getConfigByTitle } from "@/utils/getConfigByTitle";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Desktop = () => {
   const { openedWindows } = useSelector((state: Store) => state.window);
+  const dispatch = useDispatch();
   return (
-    <div id="desktop" className=" absolute">
-      {openedWindows.map((window) => {
-        const AppComponent = getConfigByTitle(window.title).engine;
+    <>
+      <div
+        className="absolute w-full h-full "
+        onClick={() => dispatch(setActiveWindow(-1))}
+      ></div>
+      <div id="desktop" className="absolute z-0">
+        {openedWindows.map((window) => {
+          const AppComponent = getConfigByTitle(window.title).engine;
 
-        return (
-          <div key={window.id} className="absolute">
-            <AppComponent id={window.id} />
-          </div>
-        );
-      })}
-    </div>
+          return (
+            <div key={window.id} className="absolute bg-black w-0 h-0">
+              <AppComponent id={window.id} />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
