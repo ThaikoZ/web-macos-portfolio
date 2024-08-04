8229cd64947b2f216cd2464160424f7baef3e2e6
Adrian Sudak <adriansudak@outlook.com>
2024-08-04 17:04:39 +0200

    style: Change dock height

--- src/components/dock/Dock.tsx
+++ src/components/dock/Dock.tsx
@@ -8,7 +8,7 @@ const Dock = () => {
   };
 
   return (
-    <div className="fixed bottom-0 w-full flex items-end justify-center h-[80px] z-30">
+    <div className="fixed bottom-0 w-full flex items-end justify-center h-[86px] z-30">
       <div className="flex items-center bg-dock-background w-fit h-full rounded-[29px] before:rounded-[29px] after:rounded-[29px] gap-1.5 window-border mb-1.5 back px-3 py-1.5">
         {dockApps.map((app, index) => {
           const Icon = app.appIcon;
